e8bd2215afa125fa965859f084147da3b9f4da1f
Dima Kashko <kashko_di@mail.ru>
2021-11-12 17:56:58 +0300

    feat: add cli arguments validation

--- index.js
+++ index.js
@@ -1,20 +1,54 @@
 const crypt = require('./modules/crypt');
 
 const text = 'This is secret. Message about "_" symbol!';
-const { stdout, stdin, exit } = process;
-const myArg = process.argv.slice(2);
-const allowedConfig = ['C0', 'C1', 'R0', 'R1', 'A'];
-const allowedFlags = ['-c', '--config', '-i', '--input', '-o', '--output'];
+const {
+  stdout, stdin, exit, stderr,
+} = process;
+const args = process.argv.slice(2);
 
-const getConfigValue = (flag) => {
-  const flagIndex = process.argv.indexOf(flag);
-  return flagIndex !== -1 ? process.argv[flagIndex + 1].split('-') : null;
+const hasDuplicates = (array) => new Set(array).size !== array.length;
+
+process.on('exit', (code) => {
+  if (code === 0) {
+    stdout.write('Done!\n');
+  } else {
+    stderr.write(`Что-то пошло не так. Программа завершилась с кодом ${code}\n`);
+  }
+});
+
+hasDuplicates(args) && exit(1);
+
+const getAllowedConfig = (args) => {
+  const allowedConfig = ['C0', 'C1', 'R0', 'R1', 'A'];
+  const allowedFlags = {
+    cipher: { short: '-c', long: '--config' },
+    input: { short: '-i', long: '--input' },
+    output: { short: '-o', long: '--output' },
+  };
+
+  const getConfigValue = (obj) => {
+    const shortIndex = args.indexOf(obj.short);
+    const longIndex = args.indexOf(obj.long);
+    if (shortIndex !== -1) {
+      return args[shortIndex + 1];
+    }
+    if (longIndex !== -1) {
+      return args[longIndex + 1];
+    }
+    return false;
+  };
+
+  const config = {
+    cipher: getConfigValue(allowedFlags.cipher).split('-'),
+    input: getConfigValue(allowedFlags.input),
+    output: getConfigValue(allowedFlags.output),
+  };
+
+  const validateConfig = (arr) => arr.every((item) => allowedConfig.includes(item));
+
+  const isAllowedConfig = validateConfig(config.cipher);
+  return isAllowedConfig ? config : exit(2);
 };
 
-const config = getConfigValue('-c');
-const message2 = getConfigValue('-i');
-const message3 = getConfigValue('-o');
-console.log(crypt(text, config[2]));
-console.log(config);
-console.log(message2);
-console.log(message3);
+const qwe = getAllowedConfig(args);
+console.log(qwe);
